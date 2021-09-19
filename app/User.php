36b86168a8a1ca4c319d2d4639b0b984aa4b7244
Mahmoud Jarjour <mahmoudkarake1994m@gmail.com>
2021-09-19 04:45:19 +0300

    make command (Expiration) to expire user after 1 minute

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','Mobile'
+        'name', 'email', 'password','Mobile' ,'expire'
     ];
 
     /**
